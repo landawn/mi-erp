e955aa1f8f46b05d6889f89cafc6fb356519b78b
haiyangl <lihy70@hotamil.com>
2025-12-25 22:32:24 -0800

    v3

--- src/test/java/net/mi/erp/DAOTest.java
+++ src/test/java/net/mi/erp/DAOTest.java
@@ -7,13 +7,17 @@ import org.junit.jupiter.api.Test;
 import com.landawn.abacus.jdbc.JdbcUtil;
 import com.landawn.abacus.util.N;
 
+import net.mi.erp.dao.AccountDAO;
 import net.mi.erp.dao.AddressDAO;
 import net.mi.erp.dao.EmployeeAddressDAO;
 import net.mi.erp.dao.EmployeeDAO;
+import net.mi.erp.dao.LoginDAO;
 import net.mi.erp.dao.ProjectDAO;
+import net.mi.erp.entity.Account;
 import net.mi.erp.entity.Address;
 import net.mi.erp.entity.Employee;
 import net.mi.erp.entity.EmployeeAddress;
+import net.mi.erp.entity.Login;
 import net.mi.erp.entity.Project;
 import net.mi.erp.model.UnifiedStatus;
 
@@ -22,13 +26,39 @@ public class DAOTest {
     static final String url = "jdbc:mysql://localhost:3306/mi_erp";
     static final DataSource dataSource = JdbcUtil.createHikariDataSource(url, "root", "admin");
 
+    static final AccountDAO accountDAO = JdbcUtil.createDao(AccountDAO.class, dataSource);
+    static final LoginDAO loginDAO = JdbcUtil.createDao(LoginDAO.class, dataSource);
     static final ProjectDAO projectDAO = JdbcUtil.createDao(ProjectDAO.class, dataSource);
     static final EmployeeDAO employeeDAO = JdbcUtil.createDao(EmployeeDAO.class, dataSource);
     static final AddressDAO addressDAO = JdbcUtil.createDao(AddressDAO.class, dataSource);
     static final EmployeeAddressDAO employeeAddressDAO = JdbcUtil.createDao(EmployeeAddressDAO.class, dataSource);
 
     @Test
-    public void test_dao_01() {
+    public void test_LoginDAO() {
+        Account account = Account.builder().firstName("Jack").lastName("Johnson").status(UnifiedStatus.ACTIVE).build();
+        int accountId = accountDAO.insert(account);
+        Account accountFromDB = accountDAO.gett(accountId);
+        N.println(accountFromDB);
+
+        Login login = Login.builder().accountId(accountId).loginId("Jack_Johnson").loginPassword("123").status(UnifiedStatus.ACTIVE).build();
+        int loginId = loginDAO.insert(login);
+        Login loginFromDB = loginDAO.gett(loginId);
+        N.println(loginFromDB);
+
+        loginDAO.loadJoinEntities(loginFromDB, Account.class);
+        N.println(loginFromDB);
+        N.println(loginFromDB.getAccount());
+
+        accountDAO.deleteById(accountId);
+        loginDAO.deleteById(accountId);
+
+        N.println(accountDAO.gett(accountId));
+        N.println(loginDAO.gett(loginId));
+
+    }
+
+    @Test
+    public void test_ProjectDAO() {
         Project project = Project.builder().name("proj-001").status(UnifiedStatus.ACTIVE).build();
 
         int id = projectDAO.insert(project);
@@ -43,7 +73,7 @@ public class DAOTest {
     }
 
     @Test
-    public void test_dao_02() {
+    public void test_EmployeeDAO() {
         // employeeDAO.delete(Filters.alwaysTrue());
 
         Employee employee = Employee.builder().employeeNo(123).firstName("coderA").lastName("la").status(UnifiedStatus.ACTIVE).build();
